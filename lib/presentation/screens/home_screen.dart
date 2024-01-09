96523dd20c5d7fa57d06a45ccb9f2c8e4f76dfeb
Martin Robles <mlopez@realmetrics.io>
2024-01-09 14:30:25 -0600

    first commit

---
+++ lib/presentation/screens/home_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        children: [
+          ListTile(
+            title: const Text('Cubits'),
+            subtitle: const Text('Gestor de estdo simple'),
+            trailing: const Icon(Icons.arrow_back_ios_rounded),
+            onTap: () {},
+          )
+        ],
+      ),
+    );
+  }
+}
